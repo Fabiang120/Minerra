48784ac57f6f90f33ed9c0a829246614ff323c86
Fabian Garcia <fgar877@gmail.com>
2026-08-11 03:27:29 -0400

    Set up basic html for learn and did nav border

---
+++ my-app/app/learn/page.tsx
@@ -0,0 +1,9 @@
+export default function Learn() {
+  return (
+    <section>
+      <h4>LEARN BITCOIN</h4>
+      <h2>Start here.</h2>
+      <p>Hand-Picked resources that explain Bitcoin honestly, from the absolute basics to advanced montetary thesis material and protocol internals.</p>
+    </section>
+  );
+}
